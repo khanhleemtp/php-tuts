402f6d6758e7d5197502a3eb414ddbef96cfe4cd
LdKhanh <khanhleemtp>
2020-09-11 10:27:39 +0700

    string

---
+++ 5-strings/index.php
@@ -0,0 +1,38 @@
+<?php 
+    $stringOne = 'My email is ';
+    $stringTwo = 'ldkhanh@gmail.com';
+
+    // echo $stringOne . $stringTwo;
+
+    $name = 'M a r i o';
+    $hello = "Hello $name";
+    $splitLength = 2;
+
+    // echo $hello;
+    // echo "Hello \"mario\" ";
+    // echo 'Hello "mario"'
+
+    // echo $name[1];
+
+    // echo strlen($name);
+
+    // echo strtoupper($name);
+    // echo strtolower($name);
+    
+    // echo str_replace('o', 'k', $name);
+    // echo strcmp($name, $hello);
+    
+    // $arr1 = str_split($stringOne);
+    // print_r($arr1);
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings</title>
+</head>
+<body>
+    
+</body>
+</html>
