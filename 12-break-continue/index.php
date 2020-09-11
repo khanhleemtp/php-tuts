2438501d21bedaf0c2a9c8bb2c54548da8608c61
LdKhanh <khanhleemtp>
2020-09-11 14:46:02 +0700

    12-continue-breaks

---
+++ 12-break-continue/index.php
@@ -0,0 +1,33 @@
+<?php 
+    $products = [
+		['name' => 'shiny star', 'price' => 20],
+		['name' => 'green shell', 'price' => 10],
+		['name' => 'red shell', 'price' => 15],
+		['name' => 'gold coin', 'price' => 5],
+		['name' => 'lightning bolt', 'price' => 40],
+		['name' => 'banana skin', 'price' => 2]
+    ];
+
+    foreach($products as $product) {
+        if($product['price'] == 40) {
+            break;
+        }
+        
+        if($product['price'] == 15) {
+            continue;
+    }
+        echo $product['name'] . '<br />';
+    }
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
