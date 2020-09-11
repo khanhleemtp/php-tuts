48fc7d021c1bdc547f541e57a81576fadcb5dacb
LdKhanh <khanhleemtp>
2020-09-11 16:17:43 +0700

    16-header-footer

---
+++ 16-pj-temp/templates/footer.php
@@ -0,0 +1,7 @@
+
+    <footer class="section">
+        <div class="text-center text-gray-600 text-xl">
+            Copyright @2020 LD Pizza
+        </div>
+    </footer>
+</body>
---
+++ 16-pj-temp/templates/header.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LD Pizza</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        .brand{
+            background: #cbb09c !important;
+        }
+        .brand-text{
+            color: #cbb09c !important;
+        }
+    </style>
+</head>
+<body class="bg-gray-400">
+    <nav class="bg-white shadow-sm">
+        <div class="container mx-auto flex justify-between items-center">
+            <a href="#" class="text-4xl brand-text font-bold shadow-xl">LD Pizza</a>
+        <ul id="nav-mobile" class="text-center hide-on-small-and-down">
+            <li><a href="#" class="brand text-xl text-gray-100 font-samibold p-2 rounded-lg shadow-xl">Add a Pizza</a></li>
+        </ul>
+        </div>
+    </nav>
