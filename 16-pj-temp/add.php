ba41aa689cea9b8b23abf2213fe9e883eac7f674
LdKhanh <khanhleemtp>
2020-09-11 18:22:17 +0700

    20-filter&validation

--- 16-pj-temp/add.php
+++ 16-pj-temp/add.php
@@ -8,20 +8,30 @@
         if(empty($_POST['email'])) {
             echo "Email is required" . "<br />";
         } else {
-            echo htmlspecialchars($_POST['email']);
+            $email = $_POST['email'];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo 'Email not valid' . "<br />";
+            }
+            // echo htmlspecialchars($_POST['email']);
         }
 
         // check title
         if(empty($_POST['title'])) {
             echo "Title is required" . "<br />";
         } else {
-            echo htmlspecialchars($_POST['title']);
+            $title = $_POST['title'];
+            if(!preg_match('/^[A-Za-z\s]+$/', $title)) {
+                echo 'Title must be letter and space only';
+            }
         }
         // check ingredients
         if(empty($_POST['ingredients'])) {
             echo "Ingredients is required" . "<br />";
         } else {
-            echo htmlspecialchars($_POST['ingredients']);
+            $ingredients = $_POST['ingredients'];
+            if(!preg_match('/^([A-Za-z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+                echo 'Ingredients must be saparated list';
+            }
         }
 
     }
