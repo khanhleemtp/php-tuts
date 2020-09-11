4dda7c68fe4a9b8c15c8c825e9e078dbf0505f12
LdKhanh <khanhleemtp>
2020-09-11 09:40:11 +0700

    3-first-php

---
+++ 3-first-php/index.php
@@ -0,0 +1,17 @@
+<?php
+    // echo 'Hello LD';
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my first PHP file</title>
+</head>
+<body>
+    <h1><?php echo 'Hello LD' ?></h1>
+    <!-- Sever find php tag -> run code php -> html > send to browser -->
+    <!-- <h1>Hello LD</h1> -->
+</body>
+</html>
