8c3bcefea809353ef81d2ccc835740edd31938d4
LdKhanh <khanhleemtp>
2020-09-11 11:00:14 +0700

    6-number

---
+++ 6-number/index.php
@@ -0,0 +1,43 @@
+<?php 
+
+    $radius = 25;
+    $pi = 3.14; 
+    // basic +, - , *, /, ^
+
+    // echo $pi * $radius^2;
+
+    // order operations (B I T M A S)
+
+    // echo 2 * (4 + 5) / 3;
+
+    // increment & decrement operators
+
+    // echo $radius ++;
+    // echo $radius;
+
+    // shorthand operators
+
+    // $age = 20;
+    // echo $age += 15;
+    // echo $age -= 15;
+    // echo $age *= 15;
+
+
+    // number functions
+    
+    // echo floor($pi);
+    // echo ceil($pi);
+    // echo pi();
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Numbers</title>
+</head>
+<body>
+    
+</body>
+</html>
