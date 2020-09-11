58b20e6537347aa2f474780b9fa1554e8ab954ad
LdKhanh <khanhleemtp>
2020-09-11 13:31:37 +0700

    10-booleans

---
+++ 10-booleans/index.php
@@ -0,0 +1,32 @@
+<?php 
+    // comparision booleans (true of false)
+
+    // echo true; // "1"
+    // echo false; // ""
+    
+    // numbers
+
+    // echo 5 < 20;
+    // echo 5 > 20;
+    // echo 5 == 10;
+    // echo 10 == 10;    
+    // echo 5 != 10;
+    // echo 5 <= 5;
+    // echo 5 >= 5;
+
+    // strings
+
+    // echo 'shaun' < 'yoshi';
+    // echo 'shaun' > 'yoshi';
+    // echo 'shaun' > 'Shaun';
+    // echo 'mario' == 'mario';
+    // echo 'mario' == 'Mario';
+
+    // loose vs strict equal comparison
+    // echo 5 == '5'; // true
+    // echo 5 === '5'; // false
+    
+    // echo true == '1';
+    // echo false == '';
+
+?>
