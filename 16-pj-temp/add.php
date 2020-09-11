3846f893148856d7edc63f1ffe0ef931145295fb
LdKhanh <khanhleemtp>
2020-09-11 17:56:36 +0700

    19-basic-form-validation

--- 16-pj-temp/add.php
+++ 16-pj-temp/add.php
@@ -3,9 +3,27 @@
     $labelClass = 'block text-gray-500';
 
     if(isset($_POST['submit'])) {
-        echo htmlspecialchars($_POST['email']);
-        echo htmlspecialchars($_POST['title']);
-        echo htmlspecialchars($_POST['ingredients']);
+
+        // check email
+        if(empty($_POST['email'])) {
+            echo "Email is required" . "<br />";
+        } else {
+            echo htmlspecialchars($_POST['email']);
+        }
+
+        // check title
+        if(empty($_POST['title'])) {
+            echo "Title is required" . "<br />";
+        } else {
+            echo htmlspecialchars($_POST['title']);
+        }
+        // check ingredients
+        if(empty($_POST['ingredients'])) {
+            echo "Ingredients is required" . "<br />";
+        } else {
+            echo htmlspecialchars($_POST['ingredients']);
+        }
+
     }
     // $_POST: global array is PHP
     // action="add.php" -> send to server handle
