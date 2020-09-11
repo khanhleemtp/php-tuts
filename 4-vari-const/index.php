a6cbe58c91b5a7fcd69aad64e212830dda435fc5
LdKhanh <khanhleemtp>
2020-09-11 09:55:12 +0700

    4-variable-const

---
+++ 4-vari-const/index.php
@@ -0,0 +1,18 @@
+<?php
+    define('NAME', 'Mario');
+    $name = 'Yoshi';
+    $age = 30;
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable & Const</title>
+</head>
+<body>
+    <h1>Hello PHP</h1>
+    <p><?php echo NAME; ?></p>
+    <p><?php echo $age; ?></p>
+</body>
+</html>
