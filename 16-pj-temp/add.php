df1aa1f641506ebf2481a66ad54ccdb9bd46f612
LdKhanh <khanhleemtp>
2020-09-12 08:19:36 +0700

    21-show-err

--- 16-pj-temp/add.php
+++ 16-pj-temp/add.php
@@ -2,35 +2,38 @@
     $inputClass = 'rounded-full bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500';
     $labelClass = 'block text-gray-500';
 
+    $errors = array('email'=>'', 'title'=>'', 'ingredients'=>'');
+    $email = $title = $ingredients = '';
+
     if(isset($_POST['submit'])) {
 
         // check email
         if(empty($_POST['email'])) {
-            echo "Email is required" . "<br />";
+            $errors['email'] = "Email is required" . "<br />";
         } else {
             $email = $_POST['email'];
             if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                echo 'Email not valid' . "<br />";
+            $errors['email'] = 'Email not valid' . "<br />";
             }
             // echo htmlspecialchars($_POST['email']);
         }
 
         // check title
         if(empty($_POST['title'])) {
-            echo "Title is required" . "<br />";
+            $errors['title'] = "Title is required" . "<br />";
         } else {
             $title = $_POST['title'];
             if(!preg_match('/^[A-Za-z\s]+$/', $title)) {
-                echo 'Title must be letter and space only';
+                $errors['title'] = 'Title must be letter and space only';
             }
         }
         // check ingredients
         if(empty($_POST['ingredients'])) {
-            echo "Ingredients is required" . "<br />";
+            $errors['ingredients'] = "Ingredients is required" . "<br />";
         } else {
             $ingredients = $_POST['ingredients'];
             if(!preg_match('/^([A-Za-z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
-                echo 'Ingredients must be saparated list';
+                $errors['ingredients'] =  'Ingredients must be saparated list';
             }
         }
 
@@ -48,11 +51,14 @@
             class="bg-white flex flex-col align-center justify-center p-4 rounded-lg"
             >
             <label class="<?php echo $labelClass ?>">Your Email</label>
-            <input type="text" name="email" class="<?php echo $inputClass ?>" />
+            <input type="text" name="email" class="<?php echo $inputClass ?>" value="<?php echo $email ?>"/>
+            <div class="text-red-300"><?php echo $errors['email'] ?></div>
             <label class="<?php echo $labelClass ?>">Pizza Title</label>
-            <input type="text" name="title" class="<?php echo $inputClass ?>"/>
+            <input type="text" name="title" class="<?php echo $inputClass ?>" value="<?php echo $title ?>"/>
+            <div class="text-red-300"><?php echo $errors['title'] ?></div>
             <label class="<?php echo $labelClass ?>">Ingredients (comma separated):</label>
-            <input type="text" name="ingredients" class="<?php echo $inputClass ?>" />
+            <input type="text" name="ingredients" class="<?php echo $inputClass ?>" value="<?php echo $ingredients ?>" />
+            <div class="text-red-300"><?php echo $errors['ingredients'] ?></div>
             <div class="text-center">
                 <input type="submit" value="Submit" name="submit" class="brand text-xl text-gray-100 font-samibold p-2 px-6 mt-4 rounded-lg shadow-xl" />
             </div>
