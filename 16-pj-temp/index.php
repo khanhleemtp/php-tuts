9d0c41d703a966c18b73e63c56529b901a721100
LdKhanh <khanhleemtp>
2020-09-12 14:57:46 +0700

    26-getting-data-from-db

--- 16-pj-temp/index.php
+++ 16-pj-temp/index.php
@@ -3,6 +3,27 @@
     if(!$conn) {
         echo 'Connect is failed' . mysqli_connect_error();
     }
+
+    // write query from all pizzas
+
+    $sql = 'SELECT title, ingredients, id FROM pizzas';
+
+    // make query and get result
+
+    $result = mysqli_query($conn, $sql);
+
+    // fetch the resulting rows as an array
+
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    // free result from memory
+    mysqli_free_result($result);
+    
+    // close connection
+    mysqli_close($conn);
+
+    print_r($pizzas);
+
 ?>
 
 <html lang="en">
